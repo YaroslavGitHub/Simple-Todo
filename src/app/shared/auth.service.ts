a75a7717644c24abfac7f530863f91d38bd30258
Yaroslav <4cast@ukr.net>
2020-03-26 21:48:23 +0200

    start working with save token to localstorage

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -4,28 +4,41 @@ import { User } from '../shared/interfaces';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import{ tap} from 'rxjs/operators'
-import { FbAuthResponse } from 'src/environments/interface';
+import { FbAuthResponse } from '../shared/interfaces';
 @Injectable()
 export class AuthService {
   constructor (private http: HttpClient) {}
     get token(): string {
-      return ''
+      const expDate = new Date(localStorage.getItem('fb-token-exp'))
+      if (new Date() > expDate) {
+        this.logout()
+        return null
+      }
+      return localStorage.getItem('fb-token')
     }
 
     login(user: User): Observable<any> {
+      user.returnSecureToken = true
       return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
       .pipe(
         tap(this.setToken)
       )
     }
     logout() {
-
+      this.setToken(null)
     }
     isAuthenticated(): boolean {
       return !!this.token
     }
-    private setToken(res: FbAuthResponse) {
-      console.log(res)
+    private setToken(res: FbAuthResponse | null) {
+      if (res) {
+        const expDate = new Date(new Date().getTime() + +res.expiresIn * 1000)
+      localStorage.setItem('fb-token', res.idToken)
+      localStorage.setItem('fb-token-exp', expDate.toString())
+      } else {
+        localStorage.clear()
+      }
+
     }
 
 }
